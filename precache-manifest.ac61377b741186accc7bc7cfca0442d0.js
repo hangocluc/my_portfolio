e0a6df56ff5685d9ecb212dff5c61499dc6da813
luchn <hangocluc2001@gmail.com>
2024-08-16 11:18:51 +0700

    Updates

---
+++ precache-manifest.ac61377b741186accc7bc7cfca0442d0.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a589a6aea4929cb8d0a25d1486ded4d0",
+    "url": "/hangocluc/my_portfolio/index.html"
+  },
+  {
+    "revision": "44dacb30e6b659dca7c1",
+    "url": "/hangocluc/my_portfolio/static/css/2.4a711fb7.chunk.css"
+  },
+  {
+    "revision": "fff5a34f0946a51e725d",
+    "url": "/hangocluc/my_portfolio/static/css/main.aee7d4d4.chunk.css"
+  },
+  {
+    "revision": "44dacb30e6b659dca7c1",
+    "url": "/hangocluc/my_portfolio/static/js/2.e5f18dbc.chunk.js"
+  },
+  {
+    "revision": "7655a5cddd3ed4dc84c4ef2f95e356e9",
+    "url": "/hangocluc/my_portfolio/static/js/2.e5f18dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff5a34f0946a51e725d",
+    "url": "/hangocluc/my_portfolio/static/js/main.4c72648d.chunk.js"
+  },
+  {
+    "revision": "b073cdefdd8995d005df",
+    "url": "/hangocluc/my_portfolio/static/js/runtime-main.f898019a.js"
+  },
+  {
+    "revision": "6c10af29deea59105553660864c4b2e3",
+    "url": "/hangocluc/my_portfolio/static/media/1.1.6c10af29.PNG"
+  },
+  {
+    "revision": "a90ef6fa720da9b484ff553a36c4838c",
+    "url": "/hangocluc/my_portfolio/static/media/1.2.a90ef6fa.PNG"
+  },
+  {
+    "revision": "37311a52348c5d4e7ff1202d23c2b8e0",
+    "url": "/hangocluc/my_portfolio/static/media/1.3.37311a52.PNG"
+  },
+  {
+    "revision": "02697d37f72c1086ea7943ac1a1c2236",
+    "url": "/hangocluc/my_portfolio/static/media/1.4.02697d37.PNG"
+  },
+  {
+    "revision": "664f75392ad3789df8e1b0f55633132c",
+    "url": "/hangocluc/my_portfolio/static/media/1.5.664f7539.PNG"
+  },
+  {
+    "revision": "6a87fd05aea4cc8f22dbaf17d0ffa231",
+    "url": "/hangocluc/my_portfolio/static/media/1.6.6a87fd05.PNG"
+  },
+  {
+    "revision": "7641732a0c515178feb76cbb35dd2c11",
+    "url": "/hangocluc/my_portfolio/static/media/2.1.7641732a.PNG"
+  },
+  {
+    "revision": "a5a7cc76aec22f31eb160849497198b2",
+    "url": "/hangocluc/my_portfolio/static/media/2.2.a5a7cc76.PNG"
+  },
+  {
+    "revision": "00961f1be11e3134155ec1f33fd390e2",
+    "url": "/hangocluc/my_portfolio/static/media/2.3.00961f1b.PNG"
+  },
+  {
+    "revision": "e0d5d01c882c72cfeabb57684b707486",
+    "url": "/hangocluc/my_portfolio/static/media/2.4.e0d5d01c.PNG"
+  },
+  {
+    "revision": "967942b44e9c1278eac3a6fee65665f0",
+    "url": "/hangocluc/my_portfolio/static/media/2.5.967942b4.PNG"
+  },
+  {
+    "revision": "68360f84194ab428f9fc1d40de608955",
+    "url": "/hangocluc/my_portfolio/static/media/2.6.68360f84.PNG"
+  },
+  {
+    "revision": "965bdef1122908785e62a27feb1783f3",
+    "url": "/hangocluc/my_portfolio/static/media/3.1.965bdef1.PNG"
+  },
+  {
+    "revision": "2c22f34748c48b505f1aee27d731b1d7",
+    "url": "/hangocluc/my_portfolio/static/media/3.2.2c22f347.PNG"
+  },
+  {
+    "revision": "010772168cf007fc94e146221fa07e42",
+    "url": "/hangocluc/my_portfolio/static/media/3.3.01077216.PNG"
+  },
+  {
+    "revision": "0330d7b98dadee051cae0a9ec252df9c",
+    "url": "/hangocluc/my_portfolio/static/media/3.4.0330d7b9.PNG"
+  },
+  {
+    "revision": "7b8699a69ed7fb23bd25acac967572fd",
+    "url": "/hangocluc/my_portfolio/static/media/3.5.7b8699a6.PNG"
+  },
+  {
+    "revision": "ba8301937df892855534420ff4f57769",
+    "url": "/hangocluc/my_portfolio/static/media/3.6.ba830193.PNG"
+  },
+  {
+    "revision": "4cb3d14302d8b0b1eb0212c6927427c0",
+    "url": "/hangocluc/my_portfolio/static/media/avatar.4cb3d143.jpg"
+  },
+  {
+    "revision": "a1f11f7409d5560eb17bd313937fe083",
+    "url": "/hangocluc/my_portfolio/static/media/developer.a1f11f74.png"
+  },
+  {
+    "revision": "5fd889765a2dda24f3b431af213c72f5",
+    "url": "/hangocluc/my_portfolio/static/media/mf-avatar.5fd88976.svg"
+  }
+]);
